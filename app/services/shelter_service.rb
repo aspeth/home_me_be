527c72ecc966eb3bbe7ee99c619d2f21649f3c12
Andrew Speth <speth.andrew@yahoo.com>
2022-06-01 20:28:06 -0600

    refactor to use parent class ShelterService

--- app/services/shelter_service.rb
+++ app/services/shelter_service.rb
@@ -1,6 +1,6 @@
-class ShelterService
+class ShelterService < BaseService
   def self.get_all_shelters
-    response = Faraday.get("https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Public_Service_WebMercator/MapServer/25/query?where=1%3D1&outFields=*&outSR=4326&f=json")
-    shelters = JSON.parse(response.body, symbolize_names: true)[:features]
+    response = conn("https://maps2.dcgis.dc.gov").get("/dcgis/rest/services/DCGIS_DATA/Public_Service_WebMercator/MapServer/25/query?where=1%3D1&outFields=*&outSR=4326&f=json")
+    get_json(response)[:features]
   end
 end
