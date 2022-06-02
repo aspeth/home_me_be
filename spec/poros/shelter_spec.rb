73d913af38216c43470ffd694b6d841877c7a09a
Andrew Speth <speth.andrew@yahoo.com>
2022-06-01 20:24:53 -0600

    update data in poro test to be more like JSON response

--- spec/poros/shelter_spec.rb
+++ spec/poros/shelter_spec.rb
@@ -2,7 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Shelter do
   it "exists", :vcr do
-    shelter = Shelter.new(attributes: { id: 1, name: "Helpful Shelter", provider: "DHS", address: "123 Main St.", city: "Denver", state: "CO", zipcode: "80201", type: "Youth", sub_type: "Low Barrier", number_of_beds: 10, lgbtq_focused: true, ages_served: "<= 18", how_to_access: "Call or walk in", onsite_medical_clinic: true, website: "helpfulshelter.org"})
+    data = {attributes: { id: 1,
+                          name: "Helpful Shelter",
+                          provider: "DHS",
+                          address: "123 Main St.",
+                          city: "Denver",
+                          state: "CO",
+                          zipcode: "80201",
+                          type: "Youth",
+                          sub_type:"Low Barrier",
+                          number_of_beds: 10,
+                          lgbtq_focused: true,
+                          ages_served: "<= 18",
+                          how_to_access: "Call or walk in",
+                          onsite_medical_clinic: true,
+                          website: "helpfulshelter.org"}}
+
+    shelter = Shelter.new(data)
 
     expect(shelter).to be_an_instance_of(Shelter)
   end
