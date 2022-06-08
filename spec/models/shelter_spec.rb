29e1b40ff895d3f529bc4a9a82df780381c01261
Andrew Speth <speth.andrew@yahoo.com>
2022-06-08 11:36:05 -0600

    add model tests for validations

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Shelter, type: :model do
-  it { should have_many(:user_shelters) }
-  it { should have_many(:users).through(:user_shelters) }
+  describe "relationships" do
+    it { should have_many(:user_shelters) }
+    it { should have_many(:users).through(:user_shelters) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_many(:user_shelters) }
-  it { should have_many(:shelters).through(:user_shelters) }
+  describe "relationships" do
+    it { should have_many(:user_shelters) }
+    it { should have_many(:shelters).through(:user_shelters) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+  end
 end
