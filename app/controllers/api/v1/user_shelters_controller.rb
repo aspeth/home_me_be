180d75faac989bcee4845aa2295615bd70c93704
Andrew Speth <speth.andrew@yahoo.com>
2022-06-10 09:58:13 -0600

    remove pry

--- app/controllers/api/v1/user_shelters_controller.rb
+++ app/controllers/api/v1/user_shelters_controller.rb
@@ -12,7 +12,6 @@ class Api::V1::UserSheltersController < ApplicationController
   def create
     shelter_id = params[:data][0][:shelter_id]
     user_id = params[:data][0][:user_id]
-    require 'pry'; binding.pry
     User.create!(email: params[:email])
     UserShelter.create!(user_id: user_id, shelter_id: shelter_id)
     render json: { data: { message: "Shelter Saved!" } }, status: 201
